7944cb6f100ccc0f5dd1aef48bcfe47a6aedd47d
Roman <saloroman23@gmail.com>
2023-11-22 21:05:55 +0200

    Make each quadrotor calculate distance from others and publish it

--- src/quad_project/src/quadrotor_controller.py
+++ src/quad_project/src/quadrotor_controller.py
@@ -2,20 +2,48 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped, Pose, Point
+from math import sqrt
+from quad_project_msgs.msg import DistancesToOtherQuadrotors
 
 
 def callback_get_pose(msg):
-    # rospy.loginfo(str(msg.pose.position))
-    # Publish pose and quad number with custom msg type
-    pose_publisher.publish(msg.pose.position)
+    frame_id = msg.header.frame_id  # frame_id format: "uav1/xxx"
+    number = int(frame_id.split('/')[0].replace('uav', ''))
+    all_poses[number - 1] = msg.pose.position
 
 
 if __name__ == '__main__':
     rospy.init_node('quadrotor', anonymous=True)
-    
+
     quadrotor_number = rospy.get_param('/quadrotor_number')
+    quadrotor_count = rospy.get_param('/quadrotor_count')
 
-    pose_publisher = rospy.Publisher('/uav' + str(quadrotor_number) + '/pose', Point, queue_size=10)
-    pose_subscriber = rospy.Subscriber('/uav' + str(quadrotor_number) + '/ground_truth_to_tf/pose', PoseStamped, callback_get_pose)
+    all_poses = [None for _ in range(quadrotor_count)]
+    distances = [None for _ in range(quadrotor_count)]
 
-    rospy.spin()
+    pose_subscribers = []
+    for i in range(1, quadrotor_count + 1):
+        pose_subscribers.append(rospy.Subscriber('/uav' + str(i) + '/ground_truth_to_tf/pose', PoseStamped, callback_get_pose))
+
+    distances_publisher = rospy.Publisher('/uav' + str(quadrotor_number) + '/distances', DistancesToOtherQuadrotors, queue_size=10)
+
+    rate = rospy.Rate(0.5)
+
+    while not rospy.is_shutdown():
+        self_pose = all_poses[quadrotor_number - 1]
+        for i in range(quadrotor_count):
+            if all_poses[i] is not None:
+                distance = sqrt(
+                    (all_poses[i].x - self_pose.x) ** 2 +
+                    (all_poses[i].y - self_pose.y) ** 2 +
+                    (all_poses[i].z - self_pose.z) ** 2
+                )
+                distances[i] = distance
+        
+        message = DistancesToOtherQuadrotors()
+        message.quadrotor_number = quadrotor_number
+        message.distances = distances
+        distances_publisher.publish(message)
+        
+
+        rate.sleep()
