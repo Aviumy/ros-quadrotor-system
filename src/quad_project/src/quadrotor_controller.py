684115f9d4b3a95c0b7dc693fc97c93cb4c4f538
Roman <saloroman23@gmail.com>
2023-12-02 18:30:31 +0200

    Move cmd_vel publishing to individual drone controller

--- src/quad_project/src/quadrotor_controller.py
+++ src/quad_project/src/quadrotor_controller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import PoseStamped, Pose, Point
+from geometry_msgs.msg import PoseStamped, Pose, Point, Twist, Vector3
 from math import sqrt
 from quad_project_msgs.msg import DistancesToOtherQuadrotors
 
@@ -25,11 +25,17 @@ if __name__ == '__main__':
     for i in range(1, quadrotor_count + 1):
         pose_subscribers.append(rospy.Subscriber('/uav' + str(i) + '/ground_truth_to_tf/pose', PoseStamped, callback_get_pose))
 
+    vel_publisher = rospy.Publisher('/uav' + str(quadrotor_number) + '/cmd_vel', Twist, queue_size=10)
     distances_publisher = rospy.Publisher('/uav' + str(quadrotor_number) + '/distances', DistancesToOtherQuadrotors, queue_size=10)
 
-    rate = rospy.Rate(0.5)
+    publish_frequency = rospy.get_param('/controller_publish_frequency')
+    rate = rospy.Rate(publish_frequency)
 
     while not rospy.is_shutdown():
+        vel_msg = Twist()
+        vel_msg.linear = Vector3(z=1.0)
+        vel_publisher.publish(vel_msg)
+
         self_pose = all_poses[quadrotor_number - 1]
         for i in range(quadrotor_count):
             if all_poses[i] is not None:
